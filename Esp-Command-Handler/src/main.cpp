1e690cb74435f8193f82e5a05566ef50e2751a2c
Sander Gaarden <sandergaarden@gmail.com>
2023-01-18 18:00:47 +0100

    Included setup code.

--- Esp-Command-Handler/src/main.cpp
+++ Esp-Command-Handler/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include "CommandHandler.h"
+
+// This is for testing lol
+int lol(){
+  printf("lol\n");
+  return 0;
+}
+
+// This is also for testing lmao
+int lmao(){
+  printf("lmao\n");
+  return 0;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial) {}
+
+  AddCommand("lolCom", lol);
+  AddCommand("lmaoCom", lmao);
+  delay(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  RunCommand("lol");
+  WaitForCommand();
 }
